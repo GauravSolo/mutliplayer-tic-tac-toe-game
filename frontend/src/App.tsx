a82d024e178ac6ed38c30edffa5926993112ecb2
Gaurav Sharma <gauravsharma9339@gmail.com>
2026-03-28 15:56:47 +0530

    feat: setup nakama server and connect react client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,30 @@
-export const App = () => {
+import { useEffect } from "react"
+import client from "./nakamaClient"
+
+function App() {
+
+  useEffect(() => {
+
+    const connect = async () => {
+
+      const session = await client.authenticateDevice(
+        crypto.randomUUID()
+      )
+
+      console.log("connected user", session.user_id)
+
+    }
+
+    connect()
+
+  }, [])
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <p>Connecting to Nakama...</p>
+    </div>
   )
 }
+
+export default App
